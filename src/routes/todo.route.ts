7da88b0aa0ef681f48a44ade93ac2b8ba2bbf4fd
unknown <jec.dev.22@gmail.com>
2023-07-17 21:08:50 -0300

    Agregar y Actualizar todo.router.ts
    Se actualiza el nombre de la ruta.
    
    Se agrega nueva ruta para eliminar todos los todo completado.

--- src/routes/todo.route.ts
+++ src/routes/todo.route.ts
@@ -2,13 +2,14 @@ import express from 'express'
 import todoController from '../controllers/todo.controller'
 const router = express.Router()
 
-router.post('/todo', (req, res) => todoController.post(req, res))
+router.post('/', (req, res) => todoController.post(req, res))
 
-router.get('/todo', (_req, res) => todoController.get(res))
-router.get('/todo/:id', (req, res) => todoController.getById(req, res))
+router.get('/', (_req, res) => todoController.get(res))
+router.get('/:id', (req, res) => todoController.getById(req, res))
 
-router.put('/todo/:id', (req, res) => todoController.updateById(req, res))
+router.put('/:id', (req, res) => todoController.updateById(req, res))
 
-router.delete('/todo/:id', (req, res) => todoController.deleteById(req, res))
+router.delete('/', (_req, res) => todoController.deleteAllCompleted(res))
+router.delete('/:id', (req, res) => todoController.deleteById(req, res))
 
 export default router
